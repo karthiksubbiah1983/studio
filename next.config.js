ccbef3ab1a6cb16b41fd457ae02ee351b8654dcb
Karthikeyan Subbiah <Karthiksubbiah1983@gmail.com>
2025-09-20 14:56:55 +0000

    im facing still the same error, can you please 2 different config one fo

---
+++ next.config.js
@@ -0,0 +1,44 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+if (process.env.NODE_ENV === 'development') {
+  nextConfig.experimental = {
+    ...nextConfig.experimental,
+    allowedDevOrigins: [
+      "https://*.cloudworkstations.dev",
+      "https://*.firebase.studio",
+    ],
+  };
+}
+
+module.exports = nextConfig;
