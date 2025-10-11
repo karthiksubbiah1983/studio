1c2e0efd7d42b8e1e638d95460181f0635c3acfe
Karthikeyan Subbiah <Karthiksubbiah1983@gmail.com>
2025-10-11 21:11:15 +0000

    The line after the Semester is always Subject the line after the Subject

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -220,9 +220,9 @@ export default function Home() {
             <Table>
               <TableHeader>
                 <TableRow>
-                  <TableHead>Template Name</TableHead>
-                  <TableHead>Category</TableHead>
-                  <TableHead>Sub Category</TableHead>
+                  <TableHead>Topic</TableHead>
+                  <TableHead>Semester</TableHead>
+                  <TableHead>Subject</TableHead>
                   <TableHead>Version</TableHead>
                   <TableHead>Last Modified</TableHead>
                   <TableHead className="text-right">Actions</TableHead>
