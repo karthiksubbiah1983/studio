7675e3efe1e798cb110cb7d87e13617eb986ec4f
Karthikeyan Subbiah <Karthiksubbiah1983@gmail.com>
2025-12-14 01:09:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8653840307-73bba",
+  "appId": "1:633582832903:web:e48c6ddcbdf52481da89a2",
+  "apiKey": "AIzaSyCn68I2ZZnbidnXD1ZyVQ4MA1IqklJygQ8",
+  "authDomain": "studio-8653840307-73bba.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "633582832903"
+};
