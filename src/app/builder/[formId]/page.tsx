6e870e131d0c0796ba7eb47f40fb97b695898a1a
Karthikeyan Subbiah <Karthiksubbiah1983@gmail.com>
2025-12-29 15:18:31 +0000

    Running "npm ci --quiet --no-fund --no-audit (NODE_ENV=development)" npm

--- src/app/builder/[formId]/page.tsx
+++ src/app/builder/[formId]/page.tsx
@@ -1,16 +1,16 @@
 
 "use client";
 
-import { useEffect, useState, use } from "react";
+import { useEffect, useState } from "react";
 import { useBuilder } from "@/hooks/use-builder";
 import { Builder } from "@/components/builder/builder";
 import { useRouter } from "next/navigation";
 
 type Props = {
-    params: Promise<{ formId: string }>;
+    params: { formId: string };
 }
 export default function BuilderPage({ params }: Props) {
-    const { formId } = use(params);
+    const { formId } = params;
     const { state, dispatch } = useBuilder();
     const router = useRouter();
     const [isLoading, setIsLoading] = useState(true);
