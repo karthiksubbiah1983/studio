877cbfcd6c5cfc1c51af06ae959b1f3dd1491d36
Karthikeyan Subbiah <Karthiksubbiah1983@gmail.com>
2026-02-07 22:23:15 +0000

    where i can update the configurations

--- src/components/builder/builder.tsx
+++ src/components/builder/builder.tsx
@@ -117,7 +117,7 @@ export function Builder({ formId }: Props) {
         </Button>
         <Button variant="outline" size="sm" onClick={() => setIsRulesOpen(true)}>
             <Settings2 className="mr-1 h-4 w-4" />
-            Manage Rules
+            Rules & Configs
         </Button>
         <Button variant="outline" size="sm" onClick={() => setIsWorkflowsOpen(true)}>
             <Zap className="mr-1 h-4 w-4" />
